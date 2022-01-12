b04fa1ea6a7a5a8f3157bb03ab6eced38b60841b
KurenkovDmitry <Dimakurenkov33557080@gmail.com>
2022-01-12 10:30:19 +0300

    new
    new

--- f.py
+++ f.py
@@ -9,7 +9,7 @@ class DB :
         self.cursor = self.connection.cursor()
         
     def write_products (self) :
-        cur = self.cursor()
+        cur = self.cursor
         cur.execute("SELECT * FROM products")
         rows = cur.fetchall()
        
@@ -19,8 +19,85 @@ class DB :
         
         return otv
     
+    def write_klienti(self) :
+        cur = self.cursor
+        cur.execute("SELECT * FROM klienti")
+        rows = cur.fetchall()
+       
+        otv = []
+        for row in rows :
+            otv.append(row[1]) 
+        
+        return otv    
+    
+    def write_products_klienti(self, name) :
+        cur = self.cursor
+        cur.execute("SELECT * FROM klienti")
+        rows = cur.fetchall()
+       
+        id_name = 0
+        for row in rows :
+            if row[1] == name :
+                id_name = row[0]
+        
+        cur.execute("SELECT * FROM pocupki")
+        rows = cur.fetchall()
+        
+        otv = []
+        for row in rows :
+            if row[1] == id_name :
+                
+                otv.append([row[0], row[2], row[3]])
+          
+        cur.execute("SELECT * FROM pocupki_items")     
+        rows = cur.fetchall()
+        for i in range(len(otv)) :
+            for row in rows :
+                if row[0] == otv[i][0] :
+                    otv[i][0] = row[1]
+                    
+        cur.execute("SELECT * FROM products")
+        rows = cur.fetchall()
+        for i in range(len(otv)) :
+            for row in rows :
+                if row[0] == otv[i][0] :
+                    otv[i][0] = row[1]
+        
+        return otv         
+    
+    def zena_pokupki_za_vse_vreme(self, name) :
+
+        cur = self.cursor       
+        cur.execute("SELECT * FROM klienti")
+        rows = cur.fetchall()
+       
+        id_name = 0
+        for row in rows :
+            if row[1] == name :
+                id_name = row[0]
+        
+        cur.execute("SELECT * FROM pocupki")
+        rows = cur.fetchall()
+        
+        otv = []
+        for row in rows :
+            if row[1] == id_name :
+                
+                otv.append([row[0], row[2], row[3]])
+          
+        zena = 0
+        cur.execute("SELECT * FROM pocupki_items")     
+        rows = cur.fetchall()
+        for i in range(len(otv)) :
+            for row in rows :
+                if row[0] == otv[i][0] :
+                    zena += row[3]    
+                    
+        a = [str(zena)]
+        return a
+    
     def __del__(self) :
-        self.connect.close()    
+        self.connection.close()    
 
 p = DB('sqlite.db')
-print(p.write_products())
+print(p.zena_pokupki_za_vse_vreme('Petr Nikolaevich'))
--- unitest.py
+++ unitest.py
@@ -1,5 +1,5 @@
 import unittest
-from base import DB
+from f import DB
 
 class test(unittest.TestCase) :
     @classmethod
@@ -9,18 +9,19 @@ class test(unittest.TestCase) :
     def test_write_products(self) :
         otv = self.database.write_products()
         self.assertEqual(res, ['ogurci', 'banan', 'voda'])
-        
+    
     def test_write_klienti(self) :
         otv = self.database.write_klienti()
         self.assertEqual(res, ['Petr Nikolaevich', 'Dmitrii', 'Djonson'])
     
     def test_write_products_klienti(self) :
         otv = self.database.write_products_klienti('Petr Nikolaevich')
-        self.assertEqual(res, ['banan', 2, 9012022]['voda', 1, 9012022])
+        self.assertEqual(res, [['banan', 2, 9012022], ['voda', 1, 9012022]])
     
     def test_zena_pokupki_za_vse_vreme(self) :
         otv = self.database.zena_pokupki_za_vse_vreme('Petr Nikolaevich')
-        self.assertEqual(res, 21.89)
+        self.assertEqual(res, ['21.89'])
+    
         
 if __name__ == '__main__' :
     unittest.main()
